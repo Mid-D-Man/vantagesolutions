da85f0c17406231f5caa5fbe0dc0b5382a030324
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-06-27 09:37:40 +0100

    Create index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export { default as Header }    from './components/Header.svelte';
+export { default as Hero }      from './components/Hero.svelte';
+export { default as WhatWeDo }  from './components/WhatWeDo.svelte';
+export { default as FreeTools } from './components/FreeTools.svelte';
+export { default as Raffle }    from './components/Raffle.svelte';
+export { default as FollowUs }  from './components/FollowUs.svelte';
+export { default as Footer }    from './components/Footer.svelte';
