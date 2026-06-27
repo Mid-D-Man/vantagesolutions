1cb968da6ef5662287c447888818c3c609a1572f
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-06-27 21:55:17 +0100

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,8 @@
-export { default as Header }    from './components/Header.svelte';
-export { default as Hero }      from './components/Hero.svelte';
-export { default as WhatWeDo }  from './components/WhatWeDo.svelte';
-export { default as FreeTools } from './components/FreeTools.svelte';
-export { default as Raffle }    from './components/Raffle.svelte';
-export { default as FollowUs }  from './components/FollowUs.svelte';
-export { default as Footer }    from './components/Footer.svelte';
-
+export { default as Header }       from './components/Header.svelte';
+export { default as Hero }         from './components/Hero.svelte';
+export { default as ImageGallery } from './components/ImageGallery.svelte';
+export { default as WhatWeDo }     from './components/WhatWeDo.svelte';
+export { default as FreeTools }    from './components/FreeTools.svelte';
+export { default as Raffle }       from './components/Raffle.svelte';
+export { default as FollowUs }     from './components/FollowUs.svelte';
+export { default as Footer }       from './components/Footer.svelte';
