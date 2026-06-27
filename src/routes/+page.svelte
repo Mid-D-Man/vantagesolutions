7a9299a70eabfcdb88c365d353116b14ff366929
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-06-27 09:37:06 +0100

    Create +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Header, Hero, WhatWeDo, FreeTools, Raffle, FollowUs, Footer } from '$lib';
+</script>
+
+<svelte:head>
+  <title>Vantage Solutions</title>
+</svelte:head>
+
+<Header />
+<main>
+  <Hero />
+  <WhatWeDo />
+  <FreeTools />
+  <Raffle />
+  <FollowUs />
+</main>
+<Footer />
