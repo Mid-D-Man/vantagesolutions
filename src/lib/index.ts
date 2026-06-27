6102d78c0ee7ddc41b27c889078470610dd55240
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-06-27 11:05:11 +0100

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,4 @@ export { default as FreeTools } from './components/FreeTools.svelte';
 export { default as Raffle }    from './components/Raffle.svelte';
 export { default as FollowUs }  from './components/FollowUs.svelte';
 export { default as Footer }    from './components/Footer.svelte';
+
